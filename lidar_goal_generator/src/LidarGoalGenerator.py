20e2ff840b05dc4b2c35f49245db45966e0539b5
charlo1998 <charles19982009@hotmail.com>
2023-08-21 11:38:50 -0400

    reset pose before mission

--- lidar_goal_generator/src/LidarGoalGenerator.py
+++ lidar_goal_generator/src/LidarGoalGenerator.py
@@ -31,6 +31,9 @@ class LidarGoalGenerator:
         # Robot pose subscriber
         self.pose_sub = rospy.Subscriber('/odom', Odometry, self.pose_callback)
 
+        #Robot pose publisher for resetting position at start of mission
+        self.init_pub = rospy.Publisher('/odom', Odometry, queue_size=10)
+
         # Robot goal subscriber
         #self.goal_sub = rospy.Subscriber('/goal', Odometry, self.goal_callback)
         
@@ -149,6 +152,14 @@ class LidarGoalGenerator:
 
         self.goal_pub.publish(goal_msg)
 
+    def reset_pose(self):
+        init_msg = Odometry()
+        init_msg.pose.pose.position.x = 0
+        init_msg.pose.pose.position.y = 0
+        init_msg.pose.pose.orientation.z = 0
+        init_msg.pose.pose.orientation.w = 0
+
+        self.init_pub.publish(init_msg)
 
     def action2velocity(self, action):
         """
@@ -189,6 +200,8 @@ class LidarGoalGenerator:
         
     def run(self):
         rate = rospy.Rate(8)  # 8 Hz
+
+        self.reset_pose()
         
         while not rospy.is_shutdown():
             self.publish_goal(3,1)
@@ -197,6 +210,8 @@ class LidarGoalGenerator:
             if self.navigating_to_goal:
                 #angle_to_goal = math.atan2(self.goal_y - self.robot_y, self.goal_x - self.robot_x)
                 #angular = angle_to_goal - math.atan2(math.sin(angle_to_goal - self.robot_yaw), math.cos(angle_to_goal - self.robot_yaw))
+
+                #normalize state and act
 
                 #action, _states = self.model.predict(self.state)
 
