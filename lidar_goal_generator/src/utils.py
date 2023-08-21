8b0516f2c7a881eab06e616e69b4109dd2839d0c
charlo1998 <charles19982009@hotmail.com>
2023-08-21 15:24:23 -0400

    added precision in prints

--- lidar_goal_generator/src/utils.py
+++ lidar_goal_generator/src/utils.py
@@ -178,14 +178,13 @@ class gofai():
         #print(f"orientations: {np.round(orientations,2)}")
         #print(f"sensors: {np.round(sensors,1)}")
         #print(f"goal_distance: {global_goal_distance} angle: {goal_angle*180/math.pi}")
-        print(f"predicted destination: {[np.round(y_dest,1), np.round(x_dest,1)]}")
+        print(f"predicted destination: {[np.round(y_dest,2), np.round(x_dest,2)]}")
         #print(f"destination: {np.round(now,2)}")
         #print(f"min distance in chosen trajectory: {minDist}")
         #print(f"goal speed: {travel_speed}")
         #print(f"received speed: {np.round(np.sqrt(x_vel**2 + y_vel**2),2)}")
         print(f"received pos: {[np.round(y_pos,2), np.round(x_pos,2)]}")
         #print(f"corrected pos: {[np.round(y_pos+y_offset,2), np.round(x_pos+x_offset,2)]}")
-        #print(f"predicted destination: {[np.round(y_dest,2), np.round(x_dest,2)]}")
         #print(f"new_dist: {new_dist}")
         #---------------------------------------------
 
