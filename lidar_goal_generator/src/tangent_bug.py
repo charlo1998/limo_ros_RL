338b82df67f93e0b11aa0ddcb37b6603e702b38a
charlo1998 <charles19982009@hotmail.com>
2023-08-23 15:35:24 -0400

    heuristic prints if verbose 2

--- lidar_goal_generator/src/tangent_bug.py
+++ lidar_goal_generator/src/tangent_bug.py
@@ -276,8 +276,9 @@ class tangent_bug():
             y_obj = 0.1*math.sin(object_angle+self.arc/2)
 
             dist_obj2goal = np.sqrt((y_goal-y_obj)**2 + (x_goal-x_obj)**2)
-            print(f"dist_obj2goal + 0.1 m: {dist_obj2goal + 0.1}")
-            print(f"goal_dist: {goal_dist}")
+            if verbose:
+                print(f"dist_obj2goal + 0.1 m: {dist_obj2goal + 0.1}")
+                print(f"goal_dist: {goal_dist}")
 
             #print(f"Heuristic: {dist_obj2goal}")
             return max(0.1, dist_obj2goal + 0.1)
