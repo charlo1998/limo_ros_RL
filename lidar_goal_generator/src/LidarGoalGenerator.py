fixed normalization

lidar_goal_generator/src/LidarGoalGenerator.py
@@ -129,9 +129,9 @@ class LidarGoalGenerator:
                 sensors[i] = min(distances_by_sensor[i])
                 #sensors[i] = 10 #to remove for obstacle avoidance (to use lidar)
 
-        #normalizing values and bounding them to [-1,1]
-        sensors = np.log(np.array(sensors)+0.0001)/np.log(100) #this way gives more range to the smaller distances (large distances are less important).
-        sensors = min(1,max(-1,sensors))
+            #normalizing values and bounding them to [-1,1]
+            sensors[i] = np.log(sensors[i]+0.0001)/np.log(100) #this way gives more range to the smaller distances (large distances are less important).
+            sensors[i] = min(1,max(-1,sensors[i]))
 
         #write processed data to state
         self.state[0][0][6:18] = sensors