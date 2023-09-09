b857a4117684ceaf5c6f693a1a156f13d4227c23
charlo1998 <charles19982009@hotmail.com>
2023-09-09 14:33:17 -0400

    typo

--- lidar_goal_generator/src/LidarGoalGenerator.py
+++ lidar_goal_generator/src/LidarGoalGenerator.py
@@ -248,7 +248,10 @@ class LidarGoalGenerator:
         #print(f"wanted v_side: {v_side}")
         if v_front < -0.1: #do not allow high backwards velocities, turn around instead
             linear = 0
-            angular = v_angular*1.5 #rotate faster
+            if math.single(angle) > 0:
+                v_angular = self.angular_speed*1.5 #rotate faster
+            else:
+                v_angular = -self.angular_speed*1.5
             print(f"rotating to go backwards")
         elif abs(angle) > 45: #if we are badly aligned with the goal, rotate and go slower
             linear = v_front*0.5
--- lidar_goal_generator/src/tangent_bug.py
+++ lidar_goal_generator/src/tangent_bug.py
@@ -73,7 +73,7 @@ class tangent_bug():
         #print(f"sensors: {sensors}")
 
         for i, sensor in enumerate(sensors):
-            if sensor >= 10 and self.objects_last_updated[i] < 4:
+            if sensor >= 10 and self.objects_last_updated[i] < 12:
                 sensors[i] = self.previous_obs[i]+0.05
                 self.objects_last_updated[i] += 1
             else:
