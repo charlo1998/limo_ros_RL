aa7cc81be01c0d4c3fdd3a24d943a02ccadc6606
charlo1998 <charles19982009@hotmail.com>
2023-09-12 10:17:35 -0400

    taking robot yaw into account

--- lidar_goal_generator/src/LidarGoalGenerator.py
+++ lidar_goal_generator/src/LidarGoalGenerator.py
@@ -150,16 +150,18 @@ class LidarGoalGenerator:
 
         #First update obstacles positions
         for i in range(self.nb_of_sensors):
+            self.x_objects[i] -= dx
+            self.y_objects[i] -= dy
+            
             if len(distances_by_sensor[i]) == 0: #missing lidar values!
                 sensors[i] = 66 #with no information, set to 66, which will be ignored
-                self.x_objects[i] -= dx
-                self.y_objects[i] -= dy
+                
         
 
         #use obstacles positions to update sensors
         obstacles_by_sensor = [[] for i in range(self.nb_of_sensors)]
 
-        object_angles = np.arctan2(self.y_objects, self.x_objects)
+        object_angles = np.arctan2(self.y_objects, self.x_objects) - self.robot_yaw
         object_distances = np.sqrt(self.x_objects**2+self.y_objects**2)
         print(f"object angles: {np.round(object_angles*180/np.pi,1)}")
         print(f"object_distances: {object_distances}")
