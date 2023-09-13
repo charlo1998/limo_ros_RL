FIXED fastest rotation dir

lidar_goal_generator/src/LidarGoalGenerator.py
@@ -255,7 +255,9 @@ class LidarGoalGenerator:
         #correcting for current yaw
         angle = angle - self.robot_yaw
         if angle > np.pi:
-            angle = np.pi - angle
+            angle = angle - 2*np.pi
+        elif angle < np.pi:
+            angle = 2*np.pi - angle
         print(f"estimated yaw: {np.round(self.robot_yaw*180/np.pi,1)}")
         #print(f"wanted angle corrected for yaw: {np.round(angle*180/np.pi,1)}")
         v_front =  speed*math.cos(angle)

lidar_goal_generator/src/tangent_bug.py
@@ -171,7 +171,7 @@ class tangent_bug():
             command = self.calculate_pid_command(objects[closest_obstacle_idx])
             #print(f"PID command: {command}")
             tangent += command*self.tangent_direction
-            print(f"corrected tangent: {np.round(tangent*180/math.pi)}")
+            print(f"corrected tangent: {np.round(tangent*180/math.pi)}, tangent sign: {self.tangent_direction}")
             goal = [settings.mv_fw_spd_1*math.cos(tangent), settings.mv_fw_spd_1*math.sin(tangent)]
 
             object_to_avoid = segments[closest_obstacle_idx]