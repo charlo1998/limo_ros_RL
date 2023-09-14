6be73bd74a4bc4fdd1653a243a9a6dac089266ec
charlo1998 <charles19982009@hotmail.com>
2023-09-13 20:04:36 -0400

    goal more attractive

--- lidar_goal_generator/src/utils.py
+++ lidar_goal_generator/src/utils.py
@@ -31,7 +31,7 @@ class gofai():
 
     def __init__(self):
         self.arc = 2*math.pi/settings.number_of_sensors #rad
-        self.heading_coeff = 2
+        self.heading_coeff = 2.5
         self.safety_coeff = 0.4
         self.safety_dist = 0.75
         self.previous_obs = [3]*(settings.number_of_sensors+6)
